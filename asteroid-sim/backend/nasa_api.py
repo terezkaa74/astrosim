301a837e121e17ba349dd887be8a02c55f2e54c9
terezkaa74 <tereza.gorgolova@gmail.com>
2025-10-05 14:01:46 +0200

    Delete asteroid-sim directory

--- asteroid-sim/backend/nasa_api.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-def fetch_neo_data(neo_id, api_key):
-    url = f"https://api.nasa.gov/neo/rest/v1/neo/{neo_id}?api_key={api_key}"
-    r = requests.get(url)
-    r.raise_for_status()
-    return r.json()
